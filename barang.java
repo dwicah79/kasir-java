fd42e1872b5a2f5f6d49d488be487a7abe497387
dwicah79 <101717602+dwicah79@users.noreply.github.com>
2022-12-31 11:38:45 +0700

    Update barang.java

--- barang.java
+++ barang.java
@@ -1,7 +1,8 @@
 package tugasakhir;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
-import java.util.Arrays;
 public class barang implements kalkulator{
     private String nama,alamat;
     private int memberid;
@@ -64,50 +65,73 @@ public class barang implements kalkulator{
     }
 
     public void Barang() {
+        java.io.File file = new File("struk.txt");
         int barang;
         float bayaran;
         double akhir;
-        Scanner c = new Scanner(System.in);
-        System.out.print("MASUKAN JUMLAH BELANJA ANDA  = ");
-        barang = c.nextInt();
-        String nama[] = new String[barang];
-        int harga[] = new int[barang];
-        int jumlah[] = new int[barang];
-        int total[] = new int[barang];
-        double biaya = 0;
-        for (int a = 0; a < barang; a++) {
-            System.out.print("Nama Barang = ");
-            nama[a] = c.next();
-            System.out.print("Harga       = ");
-            harga[a] = c.nextInt();
-            System.out.print("Jumlah      = ");
-            jumlah[a] = c.nextInt();
-            total[a] = harga[a] * jumlah[a];
-            System.out.println("Total Bayar = " + total[a]);
+        try {
+
+            java.io.PrintWriter out = new PrintWriter(file);
+            Scanner c = new Scanner(System.in);
+            System.out.print("MASUKAN JUMLAH BELANJA ANDA  = ");
+            barang = c.nextInt();
+            String nama[] = new String[barang];
+            int harga[] = new int[barang];
+            int jumlah[] = new int[barang];
+            int total[] = new int[barang];
+            double biaya = 0;
+            for (int a = 0; a < barang; a++) {
+                System.out.print("Nama Barang = ");
+                nama[a] = c.next();
+                System.out.print("Harga       = ");
+                harga[a] = c.nextInt();
+                System.out.print("Jumlah      = ");
+                jumlah[a] = c.nextInt();
+                total[a] = harga[a] * jumlah[a];
+                System.out.println("Total Bayar = " + total[a]);
+                System.out.println(" ");
+                biaya = biaya + total[a];
+
+            }
+            System.out.println("---------------------------------------------------------------------------------------");
             System.out.println(" ");
-            biaya = biaya + total[a];
+            System.out.println("=======================================================================================");
+            System.out.println("\t\t\t\t\t STRUK BELANJA INDO APRIL");
+            System.out.println("=======================================================================================");
+            out.println("=======================================================================================");
+            out.println("                      STRUK BELANJA INDO APRIL");
+            out.println("=======================================================================================");
+            for (int d = 0; d < barang; d++) {
+                System.out.println("NAMA BARANG BELANJAAN = "+nama[d]);
+                System.out.println("HARGA BARANG          = "+harga[d]);
+                System.out.println("JUMLAH BARANG         = "+jumlah[d]);
+
+
+                out.println("NAMA BARANG BELANJAAN  = "+nama[d]);
+                out.println("HARGA BARANG           = "+harga[d]);
+                out.println("JUMLAH BARANG          = "+jumlah[d]);
+            }
+            System.out.println("=======================================================================================");
+            System.out.println("Total Belanjaan  = Rp." + biaya);
+            System.out.println("---------------------------------------------------------------------------------------");
+            System.out.printf("MASUKAN UANG ANDA = ");
+            System.out.printf("Rp.= ");
+            bayaran = c.nextFloat();
+            akhir = bayaran - biaya;
+            System.out.printf("KEMBALIAN ANDA   = " + "Rp." + akhir);
+            out.println("TOTAL PEMBAYARAN       = "+biaya);
+            out.println("UANG YANG ANDA MASUKAN = "+bayaran);
+            out.println("KEMBALIAN              = "+akhir);
+            out.close();
+
+        }catch (Exception e){
+            System.out.println(e);
         }
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println(" ");
-        System.out.println("=======================================================================================");
-        System.out.println("\t\t\t\t\t STRUK BELANJA INDO APRIL");
-        System.out.println("=======================================================================================");
-        for (int d = 0; d < barang; d++) {
-            System.out.println("| " + nama[d] + "\t\t\t| " + jumlah[d] + "\t | Rp." + harga[d] + "\t| Rp." + jumlah[d] * harga[d] + "\t|");
-        }
-        System.out.println("=======================================================================================");
-        System.out.println("Total Belanjaan  = Rp." + biaya);
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.printf("MASUKAN UANG ANDA = ");
-        System.out.printf("Rp.= ");bayaran = c.nextFloat();
-        akhir = bayaran-biaya;
-        System.out.printf("KEMBALIAN ANDA   = "+"Rp."+akhir);
-
-
-
-
     }
 }
+
+
+
 
 
 
